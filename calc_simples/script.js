57dd0ca534af20bee8af9301b170f4666625d74e
HzMariano <118083862+SpellBlue@users.noreply.github.com>
2023-11-26 21:18:47 -0300

    calc_simples

---
+++ calc_simples/script.js
@@ -0,0 +1,37 @@
+
+const vals = [
+    document.querySelector('#num1').value,
+    document.querySelector('#num2').value
+]
+
+const sinais = [
+    document.querySelector('#soma'),
+    document.querySelector('#subt'),
+    document.querySelector('#multi'),
+    document.querySelector('#divi')
+]
+
+let res = document.querySelector('#res')
+
+const ops = [
+    sinais[0].addEventListener('click',()=>{
+        num1 = document.querySelector('#num1').value
+        num2 = document.querySelector('#num2').value
+        res.innerHTML=(parseFloat(num1) + parseFloat(num2))
+    }),
+    sinais[1].addEventListener('click',()=>{
+        num1 = document.querySelector('#num1').value
+        num2 = document.querySelector('#num2').value
+        res.innerHTML=(parseFloat(num1) - parseFloat(num2))
+    }),
+    sinais[2].addEventListener('click',()=>{
+        num1 = document.querySelector('#num1').value
+        num2 = document.querySelector('#num2').value
+        res.innerHTML=(parseFloat(num1) * parseFloat(num2))
+    }),
+    sinais[3].addEventListener('click',()=>{
+        num1 = document.querySelector('#num1').value
+        num2 = document.querySelector('#num2').value
+        res.innerHTML=(parseFloat(num1) / parseFloat(num2))
+    })
+]
