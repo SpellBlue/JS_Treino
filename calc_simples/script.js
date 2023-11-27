461fe637ce732c26c230187778ece9d7aff5b3e8
HzMariano <118083862+SpellBlue@users.noreply.github.com>
2023-11-26 21:30:17 -0300

    alerta para campos vazios

--- calc_simples/script.js
+++ calc_simples/script.js
@@ -17,21 +17,41 @@ const ops = [
     sinais[0].addEventListener('click',()=>{
         num1 = document.querySelector('#num1').value
         num2 = document.querySelector('#num2').value
+        if((num1=='') || (num2=='')){
+        window.alert('preencha ambos os campos')
+        }
+        else{
         res.innerHTML=(parseFloat(num1) + parseFloat(num2))
+        }
     }),
     sinais[1].addEventListener('click',()=>{
         num1 = document.querySelector('#num1').value
         num2 = document.querySelector('#num2').value
+        if((num1=='') || (num2=='')){
+            window.alert('preencha ambos os campos')
+            }
+            else{
         res.innerHTML=(parseFloat(num1) - parseFloat(num2))
+    }
     }),
     sinais[2].addEventListener('click',()=>{
         num1 = document.querySelector('#num1').value
         num2 = document.querySelector('#num2').value
+        if((num1=='') || (num2=='')){
+            window.alert('preencha ambos os campos')
+            }
+            else{
         res.innerHTML=(parseFloat(num1) * parseFloat(num2))
+    }
     }),
     sinais[3].addEventListener('click',()=>{
         num1 = document.querySelector('#num1').value
         num2 = document.querySelector('#num2').value
+        if((num1=='') || (num2=='')){
+            window.alert('preencha ambos os campos')
+            }
+            else{
         res.innerHTML=(parseFloat(num1) / parseFloat(num2))
+    }
     })
 ]
